4e9390a500433fc4092af53fe1706a18ef7df135
denis <denlnnk2014@gmail.com>
2022-12-15 20:00:00 +0200

    Made some changes

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import random
 import telebot
 import telebot_token
-from translate import Translator
-from static.motivation import motivation
+from telebot import types
 
 bot = telebot.TeleBot(telebot_token.token)
 motivation_for_user = []
@@ -10,26 +9,26 @@ motivation_for_user = []
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, f'Hello, {message.from_user.first_name}')
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    get_help = types.KeyboardButton('Help')
+    motivation = types.KeyboardButton('Motivation')
+
+    markup.add(get_help, motivation)
+    bot.send_message(message.chat.id, f'Hello, {message.from_user.first_name}', reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def get_motivation(message):
-    for matches in motivation.possible_matches:
-        if matches in message.text.lower():
-            with open('static/motivation/motivation_examples', 'r') as file:
-                motivation_text = random.choice(file.readlines()).strip()
-                motivation_for_user.append(motivation_text)
-                bot.send_message(message.chat.id, f'Here is: {motivation_text}')
+    if message.text == 'Motivation':
+        with open('static/motivation/motivation_examples', 'r') as file:
+            motivation_text = random.choice(file.readlines()).strip()
+            motivation_for_user.insert(0, motivation_text)
+            bot.send_message(message.chat.id, f'Here is: {motivation_text}')
 
-    if message.text.lower() == 'translate' and len(motivation_for_user) > 0:
-        translator = Translator(to_lang='Russian')
-        translated_motivation = translator.translate(motivation_for_user[0])
-        bot.send_message(message.chat.id, translated_motivation)
-        motivation_for_user.pop(0)
-
-    elif message.text.lower() == 'translate' and len(motivation_for_user) == 0:
-        bot.send_message(message.chat.id, 'No motivation to translate')
+    elif message.text == 'Help':
+        help_message = '<b>Here you can</b>: \n1) Get motivation'
+        bot.send_message(message.chat.id, help_message, parse_mode='html')
 
 
 bot.infinity_polling()
