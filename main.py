182ab991960fc86b8a85a582ca0dd1bb9ae0c8af
denis <denlnnk2014@gmail.com>
2022-12-17 08:53:20 +0200

    Made commands and buttons processing

--- main.py
+++ main.py
@@ -9,31 +9,30 @@ bot = telebot.TeleBot(config.token)
 motivation_for_user = []
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+@bot.message_handler(commands=['start', 'help'])
+def commands_processing(message):
+    if message.text == '/start':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
 
-    get_help = types.KeyboardButton(config.help_button)
-    motivation = types.KeyboardButton(config.motivation_button)
-    dont_follow_back = types.KeyboardButton(config.unfollowers_button)
+        motivation = types.KeyboardButton(config.motivation_button)
+        dont_follow_back = types.KeyboardButton(config.unfollowers_button)
 
-    markup.add(get_help, motivation, dont_follow_back)
-    bot.send_message(message.chat.id, f'Hello, {message.from_user.first_name}', reply_markup=markup)
+        markup.add(motivation, dont_follow_back)
+        bot.send_message(message.chat.id, f'Hello, {message.from_user.first_name}', reply_markup=markup)
+    elif message.text == '/help':
+        help_message = '<b>Here you can</b>: ' \
+                       '\n1) Get motivation ' \
+                       '\n2) See who didn\'t follow you back at Instagram'
+        bot.send_message(message.chat.id, help_message, parse_mode='html')
 
 
 @bot.message_handler(content_types=['text'])
-def get_motivation(message):
+def buttons_processing(message):
     if message.text == config.motivation_button or 'motivation' in message.text.lower():
         with open('static/motivation/motivation_examples', 'r') as file:
             motivation_text = random.choice(file.readlines()).strip()
             motivation_for_user.insert(0, motivation_text)
             bot.send_message(message.chat.id, f'<b>Here is</b>: \n{motivation_text}', parse_mode='html')
-
-    elif message.text == config.help_button:
-        help_message = '<b>Here you can</b>: ' \
-                       '\n1) Get motivation ' \
-                       '\n2) See who didn\'t follow you back at Instagram'
-        bot.send_message(message.chat.id, help_message, parse_mode='html')
 
     elif message.text == config.unfollowers_button:
         bot.send_message(message.chat.id, '<b>[Info]</b>Make sure account privacy is OFF', parse_mode='html')
@@ -57,4 +56,5 @@ def get_unfollowers(message):
         bot.send_message(message.chat.id, f'{ex}')
 
 
-bot.polling()
+if __name__ == '__main__':
+    bot.polling()
