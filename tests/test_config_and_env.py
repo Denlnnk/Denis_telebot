6718becae4e7533d673334fa03c54085d4d9582b
denis <denlnnk2014@gmail.com>
2022-12-22 09:08:25 +0200

    Implemented test_config_and_env

---
+++ tests/test_config_and_env.py
@@ -0,0 +1,19 @@
+import os
+import config
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def test_is_in_config_buttons():
+    assert config.MOTIVATION_BUTTON
+    assert config.UNFOLLOWERS_BUTTON
+    assert config.CONVERT_CURRENCIES_BUTTON
+    assert config.USER_AGENT
+
+
+def test_is_in_env():
+    assert 'TOKEN' in os.environ
+    assert 'INSTA_USERNAME' in os.environ
+    assert 'INSTA_PASSWORD' in os.environ
+    assert 'API_LAYER_TOKEN' in os.environ
