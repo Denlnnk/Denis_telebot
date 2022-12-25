b21950a045505b8f017a3a3dad181a70f7e50cf9
denis <denlnnk2014@gmail.com>
2022-12-25 15:14:20 +0200

    Improve admin points functionality

--- main.py
+++ main.py
@@ -30,22 +30,42 @@ def commands_processing(message):
 
 
 @bot.callback_query_handler(
-    func=lambda call: call.data in config.LIST_OF_BUTTONS or call.data in config.LIST_OF_ADMIN_BUTTONS)
-def call_back(call):
+    func=lambda call: call.data in config.LIST_OF_USER_BUTTONS or call.data in config.LIST_OF_ADMIN_BUTTONS)
+def buttons_call_back(call):
     if call.message:
-        if call.data in config.LIST_OF_BUTTONS:
+        if call.data in config.LIST_OF_USER_BUTTONS:
             buttons_processing(call.message, button_name=call.data)
         elif call.data in config.LIST_OF_ADMIN_BUTTONS:
             admin_processing(call.message, button_name=call.data)
+
+
+@bot.callback_query_handler(func=lambda call: call.data in config.LIST_OF_POINTS)
+def points_call_back(call):
+    if call.message:
+        if call.data == config.ADMIN_POINT:
+            bot.edit_message_reply_markup(
+                chat_id=call.message.chat.id,
+                message_id=call.message.id,
+                reply_markup=ButtonsCommand().admin_second_points()
+            )
+        elif call.data == config.BACK_POINT:
+            bot.edit_message_reply_markup(
+                chat_id=call.message.chat.id,
+                message_id=call.message.id,
+                reply_markup=ButtonsCommand().admin_first_points()
+            )
+        elif call.data == config.USER_POINT:
+            bot.edit_message_reply_markup(
+                chat_id=call.message.chat.id,
+                message_id=call.message.id,
+                reply_markup=ButtonsCommand().user_buttons(call.message)
+            )
 
 
 @bot.message_handler(func=lambda message: message.from_user.id in config.ADMIN_IDS, content_types=['text'])
 def admin_processing(message, button_name: str = None):
     buttons = {
         config.ADMIN_ADD_MOTIVATION_BUTTON: AddMotivation(),
-        config.MOTIVATION_BUTTON: ButtonMotivation(),
-        config.UNFOLLOWERS_BUTTON: ButtonUnfollowers(),
-        config.CONVERT_CURRENCIES_BUTTON: ButtonConvertCurrencies()
     }
     if button_name:
         button_process = buttons[button_name]
@@ -83,5 +103,4 @@ def voice_processing(message):
 
 
 if __name__ == '__main__':
-    bot.delete_webhook()
     bot.polling()
